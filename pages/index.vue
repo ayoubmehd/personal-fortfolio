0c4184101edb543394baa21fd4de96aae2a4f391
Ayoub <ayoub@checkin.com>
2024-03-24 17:53:33 +0000

    feat: chnage landin page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="text-gray-600 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <Navbar />
+  <header class=" body-font">
+    <div class="container mx-auto flex p-5 flex-col md:flex-row items-center">
+      <!-- <Navbar /> -->
       <Logo />
-      <div class="lg:w-2/5 hidden md:inline-flex lg:justify-end ml-5 lg:ml-0">
+      <div class="w-full hidden md:inline-flex lg:justify-end ml-auto lg:ml-0">
         <button
-          class="hidden md:inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          class="hidden md:inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg    @apply bg-slate-900 text-white; bg-slatetext-lg">
           {{ $t("cta_button") }}
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             class="w-4 h-4 ml-1" viewBox="0 0 24 24">
@@ -16,32 +16,31 @@
     </div>
   </header>
 
-  <section class="text-gray-600 body-font">
+  <section class="flex items-center h-[90vh] body-font">
     <div class="container mx-auto flex px-5 py-14 md:py-24 items-center justify-center flex-col">
-      <img class="h-[25vh] mb-10 object-cover object-center rounded" alt="hero" :src="profile" />
+      <!-- <img class="h-[25vh] mb-10 object-cover object-center rounded" alt="hero" :src="profile" /> -->
       <div class="text-center lg:w-2/3 w-full">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium ">
           {{ $t("hero.heading") }}
         </h1>
-        <h1 class="title-font sm:text-2xl text-xl mb-4 text-gray-900">
-          Ayoub ELMAHDAOUI
+        <h1 class="title-font sm:text-2xl text-xl mb-4 ">
+          {{ $t("hero.subheading") }}
         </h1>
-        <div class="flex justify-center">
+        <div class="flex justify-center mb-4">
           <button
             class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             {{ $t("cta_button") }}
           </button>
-          <button
-            class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-            {{ $t("about.title") }}
-          </button>
+        </div>
+        <!-- Social network icons container -->
+        <div class="flex justify-center">
+          <!-- <SocialMediaItem icon="facebook" label="Facebook" /> -->
+          <SocialMediaItem href="https://twitter.com/0wl_97" icon="x" label="Twitter" />
+          <!-- <SocialMediaItem icon="insta" label="Instagram" /> -->
+          <SocialMediaItem href="https://www.linkedin.com/in/ayoub-mehd" icon="in" label="LinkedIn" />
+          <SocialMediaItem href="https://github.com/ayoubmehd" icon="github" label="GitHub" />
         </div>
       </div>
     </div>
   </section>
 </template>
-
-<script lang="ts" setup>
-// @ts-ignore
-import profile from "../assets/img/profile.jpeg";
-</script>
