8ebb914afbc496fcf0e7a0b50426669409d43199
Ayoub <ayoub@checkin.com>
2024-03-24 11:02:46 +0000

    chore: switch translation package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss", "@intlify/nuxt3"],
-  intlify: {
-    localeDir: "locales",
-    vueI18n: {
-      fallbackLocale: "en",
-    },
-  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
 });
